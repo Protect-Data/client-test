a25eaab6b9fd5c33938ead37a79bb7e6c41819cd
Doug <matheus.gustavo@hotmail.com.br>
2024-10-14 20:35:01 -0300

    url api backend docker

--- frontend/src/utils/keys.ts
+++ frontend/src/utils/keys.ts
@@ -1,6 +1,6 @@
 const isDev = process.env.IS_LOCAL;
 
 // const API_URL = isDev ? `http://localhost:4000` : process.env.URL_API;
-const API_URL = process.env.URL_API || "http://backend:4000";
+const API_URL = "http://backend:4000";
 
 export { API_URL };
